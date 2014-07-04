0f825ffcf0d879526a40b93e39ba8b2cfeff9ef4
setrar <setrar@valiha.com>
2014-07-03 21:31:00 -0400

    Tweaking tests

--- src/test/scala/africa/coin/crypto/hash/Protocol_specification_HashesSpec.scala
+++ src/test/scala/africa/coin/crypto/hash/Protocol_specification_HashesSpec.scala
@@ -15,19 +15,19 @@ class Protocol_specification_HashesSpec extends FreeSpec {
     val doubleHash256 = SHA256Hash.hash(SHA256Hash.hash(hello.getBytes()))
 
     "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824" - {
-      "[in first round of sha-256]" in {
+      "(in first round of sha-256)" in {
         assert(hash256.map("%02x".format(_)).mkString equals "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824")
       }
     }
     "9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50" - {
-      "[in second round of sha-256]" in {
+      "(in second round of sha-256)" in {
         assert(doubleHash256.map("%02x".format(_)).mkString equals "9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50")
       }
     }
 
     val ripemd160Hash = RIPEMD160Hash.hash(SHA256Hash.hash(hello.getBytes()))
     "b6a9c8c230722b7c748331a8b450f05566dc7d0f" - {
-      "[with ripemd-160]" in {
+      "(with ripemd-160)" in {
         assert(ripemd160Hash.map("%02x".format(_)).mkString equals "b6a9c8c230722b7c748331a8b450f05566dc7d0f")
       }
     }
